cae8a1802900f4899db6730e79f5cacba3a6536e
Jing Huang <jhuang42@ncsu.edu>
2023-02-11 09:40:38 -0500

    Added mapping annotations to Skill, Certification and Demographic objects.

--- phtalent/src/main/java/com/positivehire/phtalent/models/Demographic.java
+++ phtalent/src/main/java/com/positivehire/phtalent/models/Demographic.java
@@ -1,6 +1,7 @@
 package com.positivehire.phtalent.models;
 
 import java.util.Objects;
+import javax.persistence.*;
 
 /**
  * Creates an Demographic object for use in arbitrary Demographics.
@@ -8,7 +9,10 @@ import java.util.Objects;
  * @author Jing Huang
  */
 public class Demographic {
-
+    /** The id of this created Demographic object */
+    @Id
+    @GeneratedValue
+    private Long id;
     /** The name of this created Demographic object */
     private String email;
     /** The officeLocations of this created Demographic object */
@@ -45,7 +49,10 @@ public class Demographic {
     public Demographic() {
     }
 
-    public Demographic(String email, String officeLocations, String reportsTo, String gender, String race, String martialStatus, Integer noOfChildren, Integer numOfYearsWandOrg, String managerName, String divisionAndBusinessUnit, String education, String militartyYearsOfService, String careerGoal, String trainingHistory, String mentor, String sponsor) {
+    public Demographic(String email, String officeLocations, String reportsTo, String gender, String race,
+            String martialStatus, Integer noOfChildren, Integer numOfYearsWandOrg, String managerName,
+            String divisionAndBusinessUnit, String education, String militartyYearsOfService, String careerGoal,
+            String trainingHistory, String mentor, String sponsor) {
         this.email = email;
         this.officeLocations = officeLocations;
         this.reportsTo = reportsTo;
@@ -192,7 +199,6 @@ public class Demographic {
         this.sponsor = sponsor;
     }
 
-
     @Override
     public boolean equals(Object o) {
         if (o == this)
@@ -201,35 +207,47 @@ public class Demographic {
             return false;
         }
         Demographic demographic = (Demographic) o;
-        return Objects.equals(email, demographic.email) && Objects.equals(officeLocations, demographic.officeLocations) && Objects.equals(reportsTo, demographic.reportsTo) && Objects.equals(gender, demographic.gender) && Objects.equals(race, demographic.race) && Objects.equals(martialStatus, demographic.martialStatus) && Objects.equals(noOfChildren, demographic.noOfChildren) && Objects.equals(numOfYearsWandOrg, demographic.numOfYearsWandOrg) && Objects.equals(managerName, demographic.managerName) && Objects.equals(divisionAndBusinessUnit, demographic.divisionAndBusinessUnit) && Objects.equals(education, demographic.education) && Objects.equals(militartyYearsOfService, demographic.militartyYearsOfService) && Objects.equals(careerGoal, demographic.careerGoal) && Objects.equals(trainingHistory, demographic.trainingHistory) && Objects.equals(mentor, demographic.mentor) && Objects.equals(sponsor, demographic.sponsor);
+        return Objects.equals(email, demographic.email) && Objects.equals(officeLocations, demographic.officeLocations)
+                && Objects.equals(reportsTo, demographic.reportsTo) && Objects.equals(gender, demographic.gender)
+                && Objects.equals(race, demographic.race) && Objects.equals(martialStatus, demographic.martialStatus)
+                && Objects.equals(noOfChildren, demographic.noOfChildren)
+                && Objects.equals(numOfYearsWandOrg, demographic.numOfYearsWandOrg)
+                && Objects.equals(managerName, demographic.managerName)
+                && Objects.equals(divisionAndBusinessUnit, demographic.divisionAndBusinessUnit)
+                && Objects.equals(education, demographic.education)
+                && Objects.equals(militartyYearsOfService, demographic.militartyYearsOfService)
+                && Objects.equals(careerGoal, demographic.careerGoal)
+                && Objects.equals(trainingHistory, demographic.trainingHistory)
+                && Objects.equals(mentor, demographic.mentor) && Objects.equals(sponsor, demographic.sponsor);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(email, officeLocations, reportsTo, gender, race, martialStatus, noOfChildren, numOfYearsWandOrg, managerName, divisionAndBusinessUnit, education, militartyYearsOfService, careerGoal, trainingHistory, mentor, sponsor);
+        return Objects.hash(email, officeLocations, reportsTo, gender, race, martialStatus, noOfChildren,
+                numOfYearsWandOrg, managerName, divisionAndBusinessUnit, education, militartyYearsOfService, careerGoal,
+                trainingHistory, mentor, sponsor);
     }
 
     @Override
     public String toString() {
         return "{" +
-            " email='" + getEmail() + "'" +
-            ", officeLocations='" + getOfficeLocations() + "'" +
-            ", reportsTo='" + getReportsTo() + "'" +
-            ", gender='" + getGender() + "'" +
-            ", race='" + getRace() + "'" +
-            ", martialStatus='" + getMartialStatus() + "'" +
-            ", noOfChildren='" + getNoOfChildren() + "'" +
-            ", numOfYearsWandOrg='" + getNumOfYearsWandOrg() + "'" +
-            ", managerName='" + getManagerName() + "'" +
-            ", divisionAndBusinessUnit='" + getDivisionAndBusinessUnit() + "'" +
-            ", education='" + getEducation() + "'" +
-            ", militartyYearsOfService='" + getMilitartyYearsOfService() + "'" +
-            ", careerGoal='" + getCareerGoal() + "'" +
-            ", trainingHistory='" + getTrainingHistory() + "'" +
-            ", mentor='" + getMentor() + "'" +
-            ", sponsor='" + getSponsor() + "'" +
-            "}";
+                " email='" + getEmail() + "'" +
+                ", officeLocations='" + getOfficeLocations() + "'" +
+                ", reportsTo='" + getReportsTo() + "'" +
+                ", gender='" + getGender() + "'" +
+                ", race='" + getRace() + "'" +
+                ", martialStatus='" + getMartialStatus() + "'" +
+                ", noOfChildren='" + getNoOfChildren() + "'" +
+                ", numOfYearsWandOrg='" + getNumOfYearsWandOrg() + "'" +
+                ", managerName='" + getManagerName() + "'" +
+                ", divisionAndBusinessUnit='" + getDivisionAndBusinessUnit() + "'" +
+                ", education='" + getEducation() + "'" +
+                ", militartyYearsOfService='" + getMilitartyYearsOfService() + "'" +
+                ", careerGoal='" + getCareerGoal() + "'" +
+                ", trainingHistory='" + getTrainingHistory() + "'" +
+                ", mentor='" + getMentor() + "'" +
+                ", sponsor='" + getSponsor() + "'" +
+                "}";
     }
-
 
 }
