3c696b0ece553837bd3459bf8a432c56f702477c
Jing Huang <jhuang42@ncsu.edu>
2023-02-17 20:57:28 -0500

    Created dummy files for frontend project.

---
+++ frontend/src/base/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import EditProfile from '../subViews/EditProfile';
+
+function Home() {
+    return (
+        <>
+            <NavBar />
+            
+        </>
+    );
+}
+
+export default Home;
