6665abcbb66721f3fc86e9572d4b0497a43c09c8
Jing Huang <jhuang42@ncsu.edu>
2023-02-23 23:15:51 -0500

    Added Dropdown menu and TabsBar components in Home Page.

--- frontend/src/base/Home.js
+++ frontend/src/base/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
+import TabsBar from '../components/TabsBar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const TITLE = ['Positions', 'Talent Pipeline', 'Performance Reviews', 'Development Plans', 'Resources', 'Reports'];
 function Home() {
+
+    const [mode, setMode] = React.useState('Positions');
     return (
         <>
             <NavBar />
+            <TabsBar titles={TITLE} setMode={setMode}/>
         </>
     );
 }
--- frontend/src/components/TabsBar.js
+++ frontend/src/components/TabsBar.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import Nav from 'react-bootstrap/Nav';
+import Dropdown from 'react-bootstrap/Dropdown';
 
 /**
  * Create TabsBar component for Employee Profile Page
@@ -9,15 +11,30 @@ import Nav from 'react-bootstrap/Nav';
 const ITEMS = ['A', 'B', 'C', 'D', 'E'];
 
 function TabsBar({ titles, setMode, ...props }) {
+
+    const [show, setShow] = React.useState(false);
+
     return (
         <Nav className="justify-content-center" fill variant="tabs" defaultActiveKey="/home/profile">
             {titles ?
                 titles.map((item) => {
-                    return (
-                        <Nav.Item key={item} variant="drak" >
-                            <Nav.Link eventKey={item} onClick={() => { setMode(item) }} style={{ color: "black", fontWeight: 'bold' }}>{item}</Nav.Link>
-                        </Nav.Item>
-                    );
+                    if (item === 'Positions') {
+                        return (
+                            <Nav.Item key={item} variant="drak" >
+                                <Nav.Link eventKey={item} onClick={() => { setMode(item); setShow(true); }} style={{ color: "black", fontWeight: 'bold' }}>{item}</Nav.Link>
+                                <Dropdown.Menu show={show}>
+                                    <Dropdown.Item onClick={() => { setShow(false); }}>Action</Dropdown.Item>
+                                    <Dropdown.Item onClick={() => { setShow(false); }}>Another action</Dropdown.Item>
+                                </Dropdown.Menu>
+                            </Nav.Item>
+                        );
+                    } else {
+                        return (
+                            <Nav.Item key={item} variant="drak" >
+                                <Nav.Link eventKey={item} onClick={() => { setMode(item) }} style={{ color: "black", fontWeight: 'bold' }}>{item}</Nav.Link>
+                            </Nav.Item>
+                        );
+                    }
                 })
                 :
                 undefined
