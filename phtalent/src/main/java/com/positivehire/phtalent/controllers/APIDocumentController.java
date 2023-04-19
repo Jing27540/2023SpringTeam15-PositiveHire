f5e7245e85b0b7235d61055d16ce004b2bf198f5
iahandy <iahandy@ncsu.edu>
2023-04-19 18:18:42 -0400

    Deleted useless codes and comments in APIDocumentController

--- phtalent/src/main/java/com/positivehire/phtalent/controllers/APIDocumentController.java
+++ phtalent/src/main/java/com/positivehire/phtalent/controllers/APIDocumentController.java
@@ -1,12 +1,8 @@
 package com.positivehire.phtalent.controllers;
 
-import java.io.IOException;
 import java.util.Base64;
 
-import org.apache.catalina.connector.Response;
-import org.apache.tomcat.util.file.ConfigurationSource.Resource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.web.servlet.server.Encoding;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -17,16 +13,11 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
 
 import com.positivehire.phtalent.models.Document;
 import com.positivehire.phtalent.services.DocumentService;
-
-import io.micrometer.common.util.StringUtils;
-import jakarta.persistence.Entity;
 
 @RestController
 @CrossOrigin("http://localhost:3000/")
@@ -46,12 +37,6 @@ public class APIDocumentController extends APIController {
         // in base64 encoded string
         String removeTrailer = doc.substring(1, doc.length() - 1);
 
-        // System.out.println(removeTrailer);
-        // System.out.println("first char: " + removeTrailer.charAt(0) + " last char is:
-        // "
-        // + removeTrailer.charAt(removeTrailer.length() - 1));
-        // System.out.println("String length is " + removeTrailer.length());
-        // byte[] decodedBytes = Base64.getUrlDecoder().decode(removeTrailer);
         byte[] decodedBytes = Base64.getDecoder().decode(removeTrailer);
 
         System.out.println("Decoded bytes");
@@ -107,16 +92,6 @@ public class APIDocumentController extends APIController {
     @GetMapping("/documents/{employeeNum}")
     public ResponseEntity<ByteArrayResource> downloadFile(@PathVariable("employeeNum") final int employeeNum) {
         Document toReturn = docService.findByEmployeeNum(employeeNum);
-
-        // HttpHeaders headers = new HttpHeaders();
-        // headers.setContentType(MediaType.APPLICATION_PDF);
-        // // Here you have to set the actual filename of your pdf
-        // String filename = "resume.pdf";
-        // headers.setContentDispositionFormData(filename, filename);
-        // headers.setCacheControl("must-revalidate, post-check=0, pre-check=0");
-        // ResponseEntity<byte[]> response = new ResponseEntity<>(toReturn.getData(),
-        // headers, HttpStatus.OK);
-        // return response;
 
         return ResponseEntity.ok().contentType(MediaType.parseMediaType(MediaType.APPLICATION_PDF_VALUE))
                 .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" +
